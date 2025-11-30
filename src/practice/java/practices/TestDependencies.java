53bbc9e023abaff1908432c6c420aeb0607d87a7
Harshal Chavan <harshalchavan@Harshals-MacBook-Air.local>
2025-12-01 00:55:02 +0400

    added dependencies

---
+++ src/practice/java/practices/TestDependencies.java
@@ -0,0 +1,11 @@
+package practices;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class TestDependencies {
+    @Test
+    public void verifyDependencies(){
+        given().when().then();
+    }
+}
